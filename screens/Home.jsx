0c76894673fd87fdfe17d4b510da55d16ee826cc
iamakmal <m.akmalalavi@gmail.com>
2024-04-29 22:41:19 +0530

    implemented home and product page

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,54 +1,67 @@
 import React, { useState } from 'react';
 import { Div, Text, Input, Image, Icon, Tag } from 'react-native-magnus';
-import { View, ScrollView, StyleSheet, FlatList } from 'react-native';
+import { View, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
 import food from '../products.json';
 
-export default function Home() {
+export default function Home({navigation}) {
   const [data, setData] = useState(food.products);
+  const [Keyword, setKeyword] = useState("");
 
-  const renderProducts = data.map((data) =>{
+    const filteredData = data.filter((data) => {
+      const name = data.name.toLowerCase()
+      const category = data.category.toLowerCase()
+      const description = data.description.toLowerCase()
+      const keyword = Keyword.toLowerCase()
+  
+      return name.includes(keyword) || category.includes(keyword) || description.includes(keyword)
+    })
+
+
+  const renderProducts = filteredData.map((data) =>{
     return(
+      <TouchableOpacity onPress={() => navigation.navigate('Product', { product: data})}>
       <View key={data.id} style={styles.productContainer}>
       <Div rounded="md">
         <Image h={200} w={200} source={{ uri: data.image }} onError={(error) => {console.log(error)}}/>
       </Div>
       <Div row alignItems="center">
       <Div flex={1}>
-        <Text fontWeight="bold" fontSize="xl" mt="sm">
-          {data.name}
-        </Text>
-        <Text color="gray500" fontSize="sm">
-          {data.description}
-        </Text>
-      </Div>
-      <Div flex={1} alignItems='flex-end'>
-        <Text color="blue500" fontWeight="bold" fontSize="xl" mb="sm">
-          {data.price}
-        </Text>
-        <Div>
+      <Div>
         <Tag bg='green500'>
-          <Text color='white'>
+          <Text color='white' fontSize='xl'>
             {data.rating}
           </Text>
         </Tag>
         </Div>
+        <Text fontWeight="bold" fontSize="3xl" mt="sm">
+          {data.name}
+        </Text>
+        <Text color="gray500" fontSize="xl">
+          {data.description}
+        </Text>
+      </Div>
+      <Div flex={1} alignItems='flex-end'>
+        <Text color="blue500" fontWeight="bold" fontSize="6xl" mb="sm">
+          {data.price}
+        </Text>
       </Div>
     </Div>
     </View>
+    </TouchableOpacity>
     )
   })
 
   return (
     <ScrollView style={styles.scrollview}>
     <View style={styles.container}>
-      <Div bg="yellow400">
-        <Text fontSize="6xl" fontWeight="bold" color="gray900" mt="lg" px="md">
-          Order Your Food Before It Is Too Cold
+      <Div bg="teal500" px={50} rounded="xl" m="xl">
+        <Text fontSize="6xl" fontWeight="bold" color="gray100" mt="lg" px="md">
+          Order Your Food Now Before It Is Too <Text fontSize="6xl" fontWeight="bold" color="red300">Cold</Text>
         </Text>
         <Input
           placeholder="Search dishes, restaurants"
-          p={10}
-          m={20}
+          mb="xl"
+          
           onChangeText={text => setKeyword(text)}
           focusBorderColor="green400"
           suffix={<Icon name="search" fontFamily="Feather" />}
@@ -57,8 +70,8 @@ export default function Home() {
       <Text fontSize="xl" color="gray900" mt="lg" px="md" mb="lg">
         All Categories
       </Text>
-      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-        <View style={{ flexDirection: 'row' }}>
+        <View style={{ flexDirection: 'row', justifyContent:'center', gap:10 }}>
+        <TouchableOpacity onPress={() => setKeyword('food')}>
           <View style={{ marginRight: 16 }}>
             <Div rounded="md">
               <Image h={100} w={100} source={require('../assets/food.png')} />
@@ -67,6 +80,8 @@ export default function Home() {
               Food
             </Text>
           </View>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => setKeyword('drink')}>
           <View style={{ marginRight: 16 }}>
             <Div rounded="md">
               <Image h={100} w={100} source={require('../assets/drink.png')} />
@@ -75,6 +90,8 @@ export default function Home() {
               Drink
             </Text>
           </View>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => setKeyword('dessert')}>
           <View style={{ marginRight: 16 }}>
             <Div rounded="md">
               <Image h={100} w={100} source={require('../assets/dessert.png')} />
@@ -83,8 +100,8 @@ export default function Home() {
               Dessert
             </Text>
           </View>
+          </TouchableOpacity>
         </View>
-      </ScrollView>
     {renderProducts}
     </View>
     </ScrollView>
--- screens/Product.jsx
+++ screens/Product.jsx
@@ -1,7 +1,87 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Div, Text, Button, Tag } from 'react-native-magnus';
+import { View, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
 
-export default function Product() {
+export default function Product({ route, navigation }) {
+  const { product } = route.params
   return (
-    <div>Product</div>
+    <ScrollView style={styles.scrollview}>
+      <View style={styles.container}>
+        <Div m="sm" rounded={10} shadow='sm' p={10} bg='white'>
+          <Div row>
+            <Div
+              rounded="xl"
+              flex={1}
+              h={300}
+              w="100%"
+              mb="md"
+              alignItems='center'
+              bgMode='contain'
+              bgImg={{ uri: product.image }}
+            />
+          </Div>
+          <Div row mb={20}>
+            <Div flex={1} alignItems='flex-start'>
+              <Text fontWeight="bold" fontSize="3xl" mt="sm">
+                {product.name}
+              </Text>
+              <Text color="gray500" fontSize="xl">
+                {product.description}
+              </Text>
+            </Div>
+            <Div col>
+            <Tag bg='teal500' h={50} w={50} mb={5} ml={20}>
+              <Text color='white' fontSize='xl'>
+                {product.rating}
+              </Text>
+            </Tag>
+            <Text color='black' fontSize="lg">
+                +{product.reviews}&nbsp;Reviews
+            </Text>
+            </Div>
+          </Div>
+          <Div row>
+            <Div mx="lg" flex={1} alignItems='flex-start'>
+            <Text color='teal500' fontSize="6xl" fontWeight='700' mt={10}>
+                {product.price}
+              </Text>
+            </Div>
+            <Div mx="lg" alignItems='flex-end'>
+              <Button w={300} h={50} mt="md" bg="teal500" fontSize="xl" rounded="md" >Add To Cart</Button>
+            </Div>
+          </Div>
+        </Div>
+
+      </View>
+
+    </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: 'relative'
+  },
+  divider: {
+    marginTop: 30,
+    borderColor: '#D9D9D9',
+    borderWidth: 3,
+    marginHorizontal: 10,
+    marginBottom: 30
+  },
+  list: {
+    width: 'auto'
+  },
+  scrollview: {
+    flex: 1,
+    minHeight: '100%'
+  },
+  scrollContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  }
+
+});
